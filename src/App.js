86545066d310e41c30038344d2256acb19a7c089
Sean Charoutanh <sean@Seans-MacBook-Pro.local>
2020-07-08 10:56:19 -0600

    added more state

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
   state = {
     persons: [
       {name: 'Jerry', age: 75},
-      {name: 'Dak', age: 23}
+      {name: 'Dak', age: 23},
+      {name: 'Zeke', age: 24}
     ]
   }
 
@@ -16,7 +17,8 @@ class App extends Component {
         <h1>Hi, I'm testing the App!</h1>
         <button>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Football</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My Hobbies: Football</Person>
       </div>
     );
   }
