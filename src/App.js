316a45f98b05cc87bbd64da6d34947ad5d78d939
Sean Charoutanh <sean@Seans-MacBook-Pro.local>
2020-07-09 22:05:12 -0600

    Handling Content the JavaScript way

--- src/App.js
+++ src/App.js
@@ -49,28 +49,35 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null;
+
+    if ( this.state.showPersons ) {
+      persons = (
+        <div>
+            <Person 
+              name={this.state.persons[0].name} 
+              age={this.state.persons[0].age} />
+            <Person 
+              name={this.state.persons[1].name} 
+              age={this.state.persons[1].age}
+              click={this.switchNameHandler.bind(this, 'Jerry Jones 2')} 
+              changed={this.nameChangeHandler} >My Hobbies: Football</Person>
+            <Person 
+              name={this.state.persons[2].name}
+              age={this.state.persons[2].age} />
+          </div> 
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm testing the App!</h1>
         <button 
           style={style}
           onClick={this.togglePersonsHandler}>Switch Name</button>
-        { 
-          this.state.showPersons === true ? 
-            <div>
-              <Person 
-                name={this.state.persons[0].name} 
-                age={this.state.persons[0].age} />
-              <Person 
-                name={this.state.persons[1].name} 
-                age={this.state.persons[1].age}
-                click={this.switchNameHandler.bind(this, 'Jerry Jones 2')} 
-                changed={this.nameChangeHandler} >My Hobbies: Football</Person>
-              <Person 
-                name={this.state.persons[2].name}
-                age={this.state.persons[2].age} />
-            </div> : null
-        }
+
+        {persons}  
+        
       </div>
     );
   }
