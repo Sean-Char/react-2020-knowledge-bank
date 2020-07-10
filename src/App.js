bafe9f6c923cf6239f4d48f646b572df1e6e0d21
Sean Charoutanh <sean@Seans-MacBook-Pro.local>
2020-07-10 10:42:39 -0600

    Setting Styles and Class Name Dynamically

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ class App extends Component {
   render() {
 
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -68,11 +68,22 @@ class App extends Component {
           })}
         </div> 
       );
+
+      style.backgroundColor = 'red';
+    }
+
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red'); // classes = ['red']
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold'); // classes = ['red','bold']
     }
 
     return (
       <div className="App">
         <h1>Hi, I'm testing the App!</h1>
+        <p className={classes.join(' ')}>My second sentence on the page</p>
         <button 
           style={style}
           onClick={this.togglePersonsHandler}>Switch Name</button>
