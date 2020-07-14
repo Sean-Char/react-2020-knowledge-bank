02c082a6c2df2445e428c67af29460230f764be5
Sean Charoutanh <sean@Seans-MacBook-Pro.local>
2020-07-13 23:21:42 -0600

    Component update lifecycle for state changes

--- src/containers/App.js
+++ src/containers/App.js
@@ -24,10 +24,18 @@ class App extends Component {
     console.log('[App.js] getDerivedStateFromProps', props);
     return state;
   }
-  
 
   componentDidMount() {
     console.log('[App.js] coponentDidMount');
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
+  }
+
+  componentDidUpdate() {
+    console.log('[App.js] componentDidUpdsate');
   }
 
   nameChangeHandler = (event, id) => {
