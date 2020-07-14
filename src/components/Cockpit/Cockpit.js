cb274206013d7fed68fe9b0dbd2a3f6f72944518
Sean Charoutanh <sean@Seans-MacBook-Pro.local>
2020-07-13 23:57:26 -0600

    Using useEffect in a functional component and controlling the behavior

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classes from './Cockpit.css';
 
 const cockpit = (props) => {
+    useEffect(() => {
+      console.log('[Cockpit.js] useEffect');
+      // Http request...
+      setTimeout(() => {
+        alert('Save data to cloud');
+      }, 1000);
+    }, []);
 
     const assignedClasses = [];
     let btnClass = '';
