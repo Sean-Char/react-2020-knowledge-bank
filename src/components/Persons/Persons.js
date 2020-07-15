8e999279b2bb46d0fb1a9e323ceeffbeb3ff26df
Sean Charoutanh <sean@Seans-MacBook-Pro.local>
2020-07-14 22:17:54 -0600

    using shouldComponentUpdate for optimization & optimizing functional component with React.memo

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -13,7 +13,13 @@ class Persons extends Component {
 
     shouldComponentUpdate(nextProps, nextState) {
         console.log('[Persons.js] shouldCompnentUpdate');
-        return true;
+        if (nextProps.persons === this.props.persons) {
+            return true;
+        }
+        else {
+            return false;
+        }
+        
     }
 
     getSnapshotBeforeUpdate(prevProps, prevState) {
